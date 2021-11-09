f6dfdb9c6c44da0eb146e0aa663e03c57bf4e569
bb.cui <bb.cui@foxmail.com>
2021-11-09 14:39:45 +0800

    切换到32位系统上：实现内核打印接口

---
+++ OSPrj/kernel.h
@@ -0,0 +1,14 @@
+/*
+ * @Descripttion: 
+ * @version: V0.01
+ * @Author: Cuibb
+ * @Date: 2021-11-07 21:40:45
+ * @LastEditors: Cuibb
+ * @LastEditTime: 2021-11-08 22:21:33
+ */
+#ifndef KERNEL_H
+#define KERNEL_H
+
+#define NULL ((void*)0)
+
+#endif // KERNEL_H
--- OSPrj/kmain.c
+++ OSPrj/kmain.c
@@ -1,6 +1,20 @@
+/*
+ * @Descripttion: 
+ * @version: V0.01
+ * @Author: Cuibb
+ * @Date: 2021-11-07 21:40:45
+ * @LastEditors: Cuibb
+ * @LastEditTime: 2021-11-09 11:36:34
+ */
 #include "kernel.h"
+#include "screen.h"
 
 void KMain()
 {
+    int n = PrintString("HELLO D.T.OS\n");
+    
+    n = PrintString("Nice\n");
+    
+    PrintIntDec(n);
 
 }
